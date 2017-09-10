0e3826b7e3a203a34228448ac085148f4fd6b47a
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2017-09-10 21:29:33 +0300

    Update vkMessagesBot.py

--- vkMessagesBot.py
+++ vkMessagesBot.py
@@ -1,64 +1,72 @@
-import vk_api
+import vk
 import telebot
 import datetime
 from time import sleep
 
-token = '...'
+token = '416525856:AAFfqq'
 bot = telebot.TeleBot(token)
+sendUserInfoFlag = True
 def authSession(login, password):
 	# get vk session
-	vk_session = vk_api.VkApi(login, password)
-	try:
-		vk_session.auth()
-	except Exception as authorizationError:
-		print(authorizationError)
-	return vk_session.get_api()
+	Session = vk.AuthSession(app_id=000, user_login=login, user_password=password,
+                                     scope='messages')
+	return vk.API(Session)
 
 def getMessages(session):
+	global sendUserInfoFlag 
 	# get all unread messages
 	while True:
-		for msg in session.messages.get(count = 5)['items']:
+		for msg in session.messages.get(count = 4)[1:]:
 			if not msg['read_state']:
+				sendUserInfoFlag = True
 				if msg['body']:
 					sendMessageInTelegram(msg, session)
 				if 'attachments' in msg:
 					# if photo in message
-					if msg['attachments'][0]['type'] == 'photo':
-						sendMessageInTelegram(msg, session, True)
-			sleep(0.3)
+					for photos in msg['attachments']:
+						if photos['type'] == 'photo':
+							sendPhotos(photos, msg, session)
+				# mark message as read
+				markMessageAsRead(msg['mid'], session)
+			sleep(0.5)
 
 def markMessageAsRead(message, session):
 	# mark unread message as read
 	return session.messages.markAsRead(message_ids = message)
 
-def sendMessageInTelegram(message, session, is_media = False):
+def sendMessageInTelegram(message, session):
+	global sendUserInfoFlag
 	# get user firstname and lastname
-	Name = session.users.get(user_ids = message['user_id'])[0]
+	Name = session.users.get(user_ids = message['uid'])[0]
 	# get post date
 	value = datetime.datetime.fromtimestamp(message['date'])
 	date = value.strftime('%Y-%m-%d %H:%M:%S')
-	bot.send_message(chat_id = '@GroupUserName', parse_mode = 'Markdown', text = '[[{0} {1}]]\n`{2}`\n{3}'.format(Name['first_name'], 
+	bot.send_message(chat_id = '@', parse_mode = 'Markdown', text = '[[{0} {1}]]\n`{2}`\n{3}'.format(Name['first_name'], 
 		Name['last_name'],
 		date,
 		message['body']))
-	if is_media:
-		# get photo size
-		maxPhotoSize = getMaxPhotoSize(message['attachments'][0]['photo'])
-		bot.send_photo(chat_id = '@GroupUserName', photo = message['attachments'][0]['photo'][maxPhotoSize])
-	# mark message as read
-	markMessageAsRead(message['id'], session)
+	sendUserInfoFlag = False
 
-def getMaxPhotoSize(photo):
-	# get max photo size
-	size = max([int(x.split('_')[1]) for x in photo.keys() if 'photo' in x])
-	return list(filter(lambda x: str(size) in x, photo))[0] 
+def sendPhotos(photo, message, session):
+	global sendUserInfoFlag
+	# get photo size
+	if sendUserInfoFlag:
+		Name = session.users.get(user_ids = message['uid'])[0]
+		# get post date
+		value = datetime.datetime.fromtimestamp(message['date'])
+		date = value.strftime('%Y-%m-%d %H:%M:%S')
+		bot.send_message(chat_id = '@', parse_mode = 'Markdown', text = '[[{0} {1}]]\n`{2}`'.format(Name['first_name'], 
+			Name['last_name'],
+			date))
+	bot.send_photo(chat_id = '@', photo = photo['photo']['src_big'])
+	sendUserInfoFlag = False
 
 def main():
 	while True:
 		try:
 			getMessages(authSession(login = '', password = ''))
-		except Exception as ConnectionVkError:
-			print(ConnectionVkError)
+		except Exception as Error:
+			print(Error)
 
 if __name__ == '__main__':
 	main() # run
